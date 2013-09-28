73be1fd0810ca4d1dd017ca1a0ffefd0498a5adb
pateldhruv <pateld5@wit.edu>
2013-09-28 15:08:55 -0400

    initial commit

---
+++ register.php
@@ -0,0 +1,39 @@
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+		<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+		<title>Register</title>
+	<head>
+	
+	<body>
+		
+		<div id="wrap">
+			<div id="header">
+
+			</div>
+			
+			<div id="main">
+				<div id="login_form">
+					<h1>Get Ink'd</h1>
+					<form id="register" action="#" method="POST">
+						<input name='first' type="text" placeholder="First Name"/> <br>
+						<input name='last' type="text" placeholder="Last Name"/> <br>
+						<input name='email' type="text" placeholder="Email" /> <br>
+						<input name='password' type="password" placeholder="Password"/> <br>
+						<input name='password2' type="password" placeholder="Confirm Password"/> <br>
+						<input name='submit' type="submit" value="Register"/>
+						
+					</form>
+				</div>
+
+			</div>
+
+		</div>
+
+		<div id="footer">
+
+		</div>
+	</body>
+	
+</html>

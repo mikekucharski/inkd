bf1e739705429ddf370d371476398a11d0ba3a30
Mike Kucharski <kucharskim@wit.edu>
2013-10-17 16:33:27 -0400

    Adding four database tables

--- home.php
+++ home.php
@@ -174,7 +174,39 @@
 								</div>
 							</div>
 							
+							<div id='ink'>
+							
+								<!-- Profile Image -->
+								<div id="prof_img">
+									<img src='res/default_profile.jpg' alt='empty'/>
+								</div>
+								
+								<!-- Ink Header -->
+								<div id='ink_header'>
+									<div id='header_left'>
+										<span>Mike Kucharski</span>
+									</div>
+									<div id='header_right'>
+										<span>10/10/13 12:00 PM</span>
+									</div>
+								</div>
+								
+								<!-- Ink Post -->
+								<div id="ink_post">
+									<p>Like OMG t this post is so super c really love this new Ink'd social network!!! I wish all my friends could see this!Like OMG t this post is so super c really love this new Ink'd social network!!! I wish all my friends could see this!Like OMG t this post is so super c really love this new Ink'd social network!!! I wish all my friends could see this!</p>
+								</div>
+								
+								<!-- Ink Options -->
+								<div id="ink_options">
+									<a href='#'><span class="label-important label"><i class="icon-heart icon-white"></i></span> Like</a>
+									<a href='#'><span class="label-info label"><i class="icon-tint icon-white"></i></span> Re-Ink</a>
+									<a href='#'><span class="label-success label"><i class="icon-comment icon-white"></i></span> Comment</a>
+								</div>
+							</div>
+							
 						</div>
+						
+						
 					</div>
 				</div>
 			</div><!-- /main -->

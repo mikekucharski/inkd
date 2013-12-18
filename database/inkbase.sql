ba909e9466b44bdb3ec263915ec7f2dc4e3e60e0
Mike Kucharski <kucharskim@wit.edu>
2013-12-18 01:57:22 -0500

    Populate database with filler data

--- database/inkbase.sql
+++ database/inkbase.sql
@@ -79,5 +79,37 @@ CREATE TABLE IF NOT EXISTS like_comment (
 );
 
 /* TESTING DATA */
+/* MIKE */
 INSERT INTO user(first_name, last_name, email, password, salt) VALUES('Mike', 'Kucharski', 'mike@gmail.com', '6cfa24d2749f77631027589ed25e48c028158896bb8674db0731622b8af24d49', 'RyWE2qf6kzjdmGswc1wV');
-INSERT INTO user_info(u_id,hometown,location, school, workplace, birthday, description) VALUES('(select max(id) from user)','hometown','location','school','workplace','birthday','Hi there');
+INSERT INTO user_info(u_id,hometown,location, school, workplace, birthday, description) VALUES((select max(u_id) from user),'hometown','location','school','workplace','birthday','Hi there');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 1', '2013-11-09 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 2', '2013-11-10 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 3', '2013-11-11 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 4', '2013-11-12 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 5', '2013-11-13 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 6', '2013-11-14 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Mikes Post Fake Post 7', '2013-11-15 14:52:54');
+
+/* Dhruv */
+INSERT INTO user(first_name, last_name, email, password, salt) VALUES('Dhruv', 'Patel', 'dhruv@gmail.com', '6cfa24d2749f77631027589ed25e48c028158896bb8674db0731622b8af24d49', 'RyWE2qf6kzjdmGswc1wV');
+INSERT INTO user_info(u_id,hometown,location, school, workplace, birthday, description) VALUES((select max(u_id) from user),'hometown','location','school','workplace','birthday','Hi there');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 1', '2013-11-09 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 2', '2013-11-10 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 3', '2013-11-11 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 4', '2013-11-12 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 5', '2013-11-13 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 6', '2013-11-14 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Dhruv Post Fake Post 7', '2013-11-15 14:52:54');
+
+/* Ahsley */
+INSERT INTO user(first_name, last_name, email, password, salt) VALUES('Ashley', 'Packard', 'ashley@gmail.com', '6cfa24d2749f77631027589ed25e48c028158896bb8674db0731622b8af24d49', 'RyWE2qf6kzjdmGswc1wV');
+INSERT INTO user_info(u_id,hometown,location, school, workplace, birthday, description) VALUES((select max(u_id) from user),'hometown','location','school','workplace','birthday','Hi there');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 1', '2013-11-09 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 2', '2013-11-10 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 3', '2013-11-11 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 4', '2013-11-12 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 5', '2013-11-13 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 6', '2013-11-14 14:52:54');
+INSERT INTO post(u_id, repost_id, message, post_time) VALUES((select max(u_id) from user), -1, 'Ashley Post Fake Post 7', '2013-11-15 14:52:54');
+
+

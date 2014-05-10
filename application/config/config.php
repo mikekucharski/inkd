3d40c773918f0301c83bd236c27ec485f89e7a7e
pateldhruv <pateld5@wit.edu>
2014-05-10 13:43:44 -0400

    moving to new index file

---
+++ application/config/config.php
@@ -0,0 +1,14 @@
+<?php
+ 
+define('BASE_URL', 'http://inkd.com');
+
+ /*
+ * All database connection variables
+ */
+
+define('DB_USER', "inkd"); // db user
+define('DB_PASSWORD', "1191ele22chemmi"); // db password (mention your db password here)
+define('DB_DATABASE', "inkbase"); // database name
+define('DB_SERVER', "localhost"); 
+
+?>

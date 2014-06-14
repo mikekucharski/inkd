0fc2709e8e1abe03e0c94a65bd22dc42412bf95d
pateldhruv <pateld5@wit.edu>
2014-06-14 18:00:13 -0400

    Moved the search page to MVC

--- application/models/friend_model.php
+++ application/models/friend_model.php
@@ -15,6 +15,36 @@
 
 			return ($result !== false && $result->num_rows>0) ? true : false;
 		}
+
+		/*
+		* 	Search
+		*/
+		public function search($search){
+			$u_id= Session::get('u_id');
+			
+			//sanitize input
+			$search = $this->db->real_escape_string(trim($search));
+			$query="SELECT u_id, first_name, last_name, email FROM user 
+					WHERE (CONCAT(first_name, ' ', last_name) LIKE '%$search%') 
+					OR (email='$search')";
+			$result = $this->db->query($query);
+			if($result !== false && $result->num_rows > 0)
+			{
+				
+				while($row=$result->fetch_assoc())
+				{	
+					$row['friends'] = $this->checkIfFriend($row['u_id']);
+					$data[] = $row;
+
+				}
+				return $data;
+			}
+			else 
+			{
+				return $search;
+			}
+
+		}
 	}
 
 ?>
---
+++ application/views/search_results.php
@@ -0,0 +1,53 @@
+<div class="column container">
+	<div class='center col-sm-6 page-header'>
+		<h1>Search Results</h1>
+	</div>
+	 
+	<!---<?php include('scripts/search.php'); ?> -->
+	<?php if($all_results === null){ ?>
+		 <div class="alert alert-danger col-sm-6 center">No Search Results Found for "<?=$_GET['search']?>"</div>
+	<?php }else{
+		foreach ($all_results as $user) {
+			if($user['friends'])
+			{
+				$f_status="none";
+				$uf_status="block";
+			}
+			else
+			{
+				$f_status="block";
+				$uf_status="none";
+				
+			}
+			if(Session::get('u_id') == $user['u_id'])
+			{
+				$f_status="none";
+				$uf_status="none";
+			}
+			?>
+			<div  class="col-lg-6 center"  id="search_result">
+				<div class='col-lg-2'>
+					<img src="public/img/default_profile.jpg" /> 
+				</div>
+				<div class='col-lg-7'>
+					<a href="profile/<?=$user['u_id']?>"><p><?=$user['first_name']?> <?=$user['last_name']?></p></a>
+					<p><?=$user['email']?></p>
+				</div>
+				
+				
+				<div class='col-lg-3 no-padding'>
+					<form class='friend_form' style='display:<?=$f_status?>'>
+						<button type='submit' class='btn btn-primary'>Add Friend</button>
+						<label name="u_idf" hidden><?=$user['u_id']?></label>
+					</form>
+					
+					<form class='unfriend_form' style='display:<?=$uf_status?>'>
+						<button type='submit' class='btn btn-success'>Friends</button> 
+						<label name="u_idf" hidden><?=$user['u_id']?></label>
+					</form>
+				</div>
+			</div>
+			<?php } 
+		} ?>
+	
+</div><!-- /column -->
